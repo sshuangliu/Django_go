51de7fe07014a0d7be0cbae32ba55d6d4bd36c1b
Max <max.liu@haha.com>
2019-12-05 22:18:01 +0800

    Initial commit

---
+++ views/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#@Time  : 2019/12/5 22:12
+#@Author: max liu
+#@File  : __init__.py.py
+
+if __name__ == '__main__':
+    pass
---
+++ views/index.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#@Time  : 2019/12/5 22:12
+#@Author: max liu
+#@File  : index.py
+
+if __name__ == '__main__':
+    pass
