929b5eef85eeee7e05efef4bc25c6ca582b92c67
Max <max.liu@haha.com>
2019-12-06 13:35:45 +0800

    test_unit

--- Django_go/settings.py
+++ Django_go/settings.py
@@ -118,4 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+# 静态文件别名（相对路径） 和 绝对路径
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
--- views/index.py
+++ views/index.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-#@Time  : 2019/12/5 22:12
-#@Author: max liu
-#@File  : index.py
+# @Time  : 2019/12/5 22:12
+# @Author: max liu
+# @File  : index.py
 
-if __name__ == '__main__':
-    pass
+from django.shortcuts import render
+
+
+def index_go(request):
+    title_value = '人生苦短，我用python'
+    body_value = '哈哈哈哈啊哈哈哈'
+    return render(request, 'index.html', {'return_title': title_value, 'reutrn_body': body_value})
